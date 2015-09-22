e0f46a8ddeee1ad7b000a63721f42af07756a01a
Nikhil Nanjappa <nnanjappa@xebia.com>
2015-09-22 20:26:24 +0530

    using GD to host contact.php

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://www.googledrive.com/host/0B-cVFVOcGQASb21zRnlOVGFTbWs",
                 type: "POST",
                 data: {
                     name: name,
