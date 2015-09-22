9504571b1d3ee62c416d4a28518720b349578162
Nikhil Nanjappa <nnanjappa@xebia.com>
2015-09-23 00:01:12 +0530

    changed the php url to local php file

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://www.googledrive.com/host/0B-cVFVOcGQASb21zRnlOVGFTbWs",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
